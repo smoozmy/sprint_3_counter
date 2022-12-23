3a3f57d59a0b73f38bbbdf679d4aa337718196ea
smoozmy <96174983+smoozmy@users.noreply.github.com>
2022-12-24 02:09:59 +0300

    counter start
    create project

---
+++ s3_counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  s3_counter
+//
+//  Created by Александр Крапивин on 24.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
