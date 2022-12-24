97d2575d7fe2975e678632bc82be8a75c401d7c7
Aleksandr Krapivin <smoozmy@yandex.ru>
2022-12-24 03:14:27 +0300

    My first project Create counter

--- s3_counter/ViewController.swift
+++ s3_counter/ViewController.swift
@@ -9,9 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelText: UILabel!
+    
+    @IBOutlet weak var labelCount: UILabel!
+   
+    
+    @IBOutlet weak var buttonCount: UIButton!
+    private var count: Int = 0
+    
+    
+    @IBOutlet weak var buttonCountNull: UIButton!
+    
+    @IBAction func changeCount(_ sender: Any) {
+        count += 1
+        labelCount.text = "\(count)"
+    }
+    
+    @IBAction func changeCountToNull(_ sender: Any) {
+        count = 0
+        labelCount.text = "\(count)"
+    }
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        labelCount.text = "\(count)"
+        buttonCount.tintColor = .black
+        buttonCountNull.tintColor = .gray
     }
 
 
